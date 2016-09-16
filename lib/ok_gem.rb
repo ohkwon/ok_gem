2e0170d17ca7509244288ca22ba050f62d803424
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-09-15 18:42:25 -0700

    add sample message for gem functionality

--- lib/ok_gem.rb
+++ lib/ok_gem.rb
@@ -1,5 +1,16 @@
 require "ok_gem/version"
 
 module OkGem
-  
+  class Name
+    def self.input
+      puts "HI!"
+      puts "My name is-"
+      puts "WHAT!?"
+      puts "My name is-"
+      puts "WHO!?"
+      puts "My name is-"
+      puts "chigga chigga"
+      puts "nope"
+    end
+  end
 end
